d281431a0c3b0e29c637b147a6f0a4da6a892e17
Stan Popovic <spope.mails@gmail.com>
2024-03-14 22:28:00 +1100

    Setting up Input page for blog post(s)

---
+++ assets/js/form.js
@@ -0,0 +1,30 @@
+/* JavaScript Document */
+/* =============================================================
+ 	First Name			: 	Strahinja     					    
+ 	Last Name			: 	Popovic					            
+ 	Developer Name      : 	CodexDeveloper			        
+ 	WebMaster Email		:	spope.mails@gmail.com               
+ ============================================================== */
+
+/* ***************************************************************************************** */
+/* JavaScript CODE ************************************************************************* */
+/* ***************************************************************************************** */
+
+const SUBMIT_POST_BTN = document.querySelector("#submit-btn");
+
+SUBMIT_POST_BTN.addEventListener("click", function(e) {
+	e.preventDefault();
+	let DATA = {
+		username: document.querySelector("#userName").value,
+		title: document.querySelector("#title").value,
+		content: document.querySelector("#content").value
+	};
+
+	localStorage.setItem("data", JSON.stringify(DATA));
+	let parsedDATA = JSON.parse(localStorage.getItem("data"));
+	
+	console.log(`Username: ${parsedDATA["username"]}\nTitle: ${parsedDATA["title"]}\nContent: ${parsedDATA["content"]}`);
+
+	window.location.assign("http://127.0.0.1:5500/blog-api/blog.html");
+});
+
